d4dab4d722cceff9b022fae56eff6d90cfcefa35
Tom Gregory <t.k.gregory@gmail.com>
2020-03-22 10:23:10 +0000

    Job for building Spring Boot project.

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
