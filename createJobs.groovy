d81d4063d077618e819616bb03a98b2bd8cf41cd
Tom Gregory <t.k.gregory@gmail.com>
2020-03-28 14:42:28 +0000

    Add pipeline for Docker.

--- createJobs.groovy
+++ createJobs.groovy
@@ -20,3 +20,18 @@ pipelineJob('theme-park-job') {
         }
     }
 }
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
