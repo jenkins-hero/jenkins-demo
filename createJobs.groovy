ca404c9920bf8825b80dc318da334419ad4ec031
Tom Gregory <t.k.gregory@gmail.com>
2020-04-03 08:01:01 +0100

    Deployment to AWS support.

--- createJobs.groovy
+++ createJobs.groovy
@@ -35,3 +35,18 @@ pipelineJob('theme-park-job-docker') {
         }
     }
 }
+pipelineJob('theme-park-job-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-aws')
+                }
+            }
+        }
+    }
+}
