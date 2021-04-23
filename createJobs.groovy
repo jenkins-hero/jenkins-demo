7077d7d45ab28ae9e19bbf38f0f2c4d2f276cd34
bmribeiro <bruno.miguel.ribeiro@everis.com>
2021-04-23 17:17:32 +0100

    Update createJobs.groovy
    Pipeline Job

--- createJobs.groovy
+++ createJobs.groovy
@@ -1,8 +1,23 @@
 pipelineJob('pipelineJob') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('pipelineJob.groovy'))
-            sandbox()
-        }
+  definition {
+    cps {
+      script(readFileFromWorkspace('pipelineJob.groovy'))
+      sandbox()
     }
+  }
+}
+
+pipelineJob('theme-park-job') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url 'https://github.com/bmribeiro/jenkins-demo.git'
+          }
+          branch 'master'
+        }
+      }
+    }
+  }
 }
