064fce36f3cccb4b11bebf3025e5b959700be06c
David Braun <38089182+DavidBraun777@users.noreply.github.com>
2021-10-05 22:25:38 -0500

    updated createJobs.groovy and seedJob.xml as directed from tutorial

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/DavidBraun777/tomGregorySpringBootAppWJenkins.git'
+                    }
+                    branch 'main'
+                }
+            }
+        }
+    }
+}
