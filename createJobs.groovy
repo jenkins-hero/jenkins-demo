1c46f264a0aa071ae8cabcaf514790facce2c2c1
Manupal Singh <manupalsingh3@gmail.com>
2023-03-25 17:10:06 -0400

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/ManupalSingh/spring-boot-api-example.git'
                     }
                     branch 'master'
                 }
